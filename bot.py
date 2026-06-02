2d441064b31412a3325606e50e360008df40f461
123ii123m-web <123ii123m@gmail.com>
2026-06-02 06:51:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,10 @@
-print("bot running")
+from telegram.ext import Application, CommandHandler
+
+async def start(update, context):
+    await update.message.reply_text("ربات روشنه امیر جان!")
+
+app = Application.builder().token("8603497838:AAGm1LIeZJ9B-hBh3ry7YOy8uAWjZEmC1W8").build()
+
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
